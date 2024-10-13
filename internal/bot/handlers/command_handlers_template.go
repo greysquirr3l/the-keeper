557acd774650fadfd9728e3281b83c93cad7cdc0
Nick Campbell <s0ma@proton.me>
2024-10-12 22:14:17 -0400

    Templates.

--- internal/bot/handlers/command_handlers_template.go
+++ internal/bot/handlers/command_handlers_template.go
@@ -2,13 +2,6 @@
 
 package handlers
 
-import (
-	"fmt"
-	"the-keeper/internal/bot"
-
-	"github.com/bwmarrin/discordgo"
-)
-
 // Commented out the initialization to prevent loading anything
 // func init() {
 // 	bot.RegisterHandlerLater("handleHelpCommand", handleHelpCommand)
@@ -16,21 +9,21 @@ import (
 // }
 
 // Dummy function for handling help command, does not perform any operations
-func handleHelpCommand(s *discordgo.Session, m *discordgo.MessageCreate, args []string, cmd *bot.Command) {
-	fmt.Println("Help command handler invoked (dummy function).")
-}
+// func handleHelpCommand(s *discordgo.Session, m *discordgo.MessageCreate, args []string, cmd *bot.Command) {
+// fmt.Println("Help command handler invoked (dummy function).")
+// }
 
 // Dummy function to simulate sending general help
-func sendGeneralHelp(s *discordgo.Session, channelID string) {
-	fmt.Println("Sending general help (dummy function).")
-}
+// func sendGeneralHelp(s *discordgo.Session, channelID string) {
+// fmt.Println("Sending general help (dummy function).")
+// }
 
 // Dummy function to simulate sending specific command help
-func sendCommandHelp(s *discordgo.Session, channelID string, commandName string) {
-	fmt.Printf("Sending help for command: %s (dummy function).\n", commandName)
-}
+// func sendCommandHelp(s *discordgo.Session, channelID string, commandName string) {
+// fmt.Printf("Sending help for command: %s (dummy function).\n", commandName)
+// }
 
 // Dummy function for dumping the database, does not perform any operations
-func handleDumpDatabaseCommand(s *discordgo.Session, m *discordgo.MessageCreate, args []string, cmd *bot.Command) {
-	fmt.Println("Dumping database (dummy function).")
-}
+// func handleDumpDatabaseCommand(s *discordgo.Session, m *discordgo.MessageCreate, args []string, cmd *bot.Command) {
+// fmt.Println("Dumping database (dummy function).")
+// }
